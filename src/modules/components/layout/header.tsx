cd1283acbcefe3824103e841e602d984084fff2f
plug333 <davidemele03@gmail.com>
2023-02-02 19:49:31 +0100

    header

---
+++ src/modules/components/layout/header.tsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom';
+
+export const Header = () => {
+    return (
+        <div className='header'>
+            <Link to='/'>Homepage</Link>
+        </div>
+    )
+}
