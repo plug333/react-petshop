7bb4b66c06fd6dc6f4b02b5fc23930f910bb2f4c
plug333 <davidemele03@gmail.com>
2023-02-06 20:04:19 +0100

    removed calcAge

--- src/modules/components/layout/pages/homepage/animal-card.tsx
+++ src/modules/components/layout/pages/homepage/animal-card.tsx
@@ -7,12 +7,6 @@ type Props = {
 export const AnimalCard = (props: Props) => {
     const { animal } = props;
 
-    const calcAge = () => {
-        const yearOfBirth = new Date(animal.birthDate).getFullYear();
-        const currentYear = new Date().getFullYear();
-        return currentYear - yearOfBirth;
-    }
-
     return (
         <div className={"animal-card "}>
             <div className="preview">
@@ -20,7 +14,6 @@ export const AnimalCard = (props: Props) => {
                 <h2>{"Type: " + animal.type}</h2>
                 <h2>{"Breed: " + animal.breed}</h2>
                 <h2>{"Birth Date: " + animal.birthDate}</h2>
-                <h2>{"Age: " + calcAge()}</h2>
                 <h2>{"Created at: " + animal.created_at}</h2>
                 <h2>{"Updated at: " + animal.updated_at}</h2>
             </div>
