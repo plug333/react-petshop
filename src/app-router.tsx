3a3f9cb9f55d3be18b925c1766e77e6072ecbb49
plug333 <davidemele03@gmail.com>
2023-02-06 20:03:32 +0100

    add add animal and edit animal routes

--- src/app-router.tsx
+++ src/app-router.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { Homepage } from './modules/components/layout/pages/homepage/homepage';
 import { NotFound } from './modules/components/layout/pages/not-found/not-found';
+import { AnimalForm } from './modules/components/layout/pages/animal-form/animal-form';
 
 export const AppRoutes = () => {
     return (
         <Routes>
             <Route path='/' element={<Homepage />} />
+            <Route path='/animals/new' element={<AnimalForm />} />
+            <Route path='/:id/edit' element={<AnimalForm />} />
             <Route path='*' element={<NotFound />} />
         </Routes>
     )
