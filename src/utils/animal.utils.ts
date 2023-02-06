66e13e4ec693ebfc42e2e377303e213cfc759de2
plug333 <davidemele03@gmail.com>
2023-02-06 20:04:34 +0100

    create a prototype of new animal

---
+++ src/utils/animal.utils.ts
@@ -0,0 +1,11 @@
+import { INewAnimal } from "../model/animal";
+
+export const defaultAnimal: INewAnimal = {
+    name: null,
+    type: null,
+    breed: null,
+    birthDate: null,
+    imgUrl: null,
+    description: null,
+    pedigree: null
+}
