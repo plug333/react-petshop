a37ba166ac3cabeaa395cbd003fd3fc7e4ab2fe1
plug333 <davidemele03@gmail.com>
2023-02-07 20:06:27 +0100

    modified the card and added the botton for details

--- src/modules/components/layout/pages/homepage/animal-card.tsx
+++ src/modules/components/layout/pages/homepage/animal-card.tsx
@@ -1,4 +1,5 @@
 import { IAnimal } from "../../../../../model/animal";
+import { Link } from "react-router-dom";
 
 type Props = {
     animal: IAnimal;
@@ -10,12 +11,13 @@ export const AnimalCard = (props: Props) => {
     return (
         <div className={"animal-card "}>
             <div className="preview">
+                <img src={animal.imgUrl} alt='animal photo' />
                 <h2>{"Name: " + animal.name}</h2>
                 <h2>{"Type: " + animal.type}</h2>
                 <h2>{"Breed: " + animal.breed}</h2>
                 <h2>{"Birth Date: " + animal.birthDate}</h2>
-                <h2>{"Created at: " + animal.created_at}</h2>
-                <h2>{"Updated at: " + animal.updated_at}</h2>
+                <h2>{"Pedigree: " + animal.pedigree}</h2>
+                <Link to={`/animal/${animal._id}`}>Details</Link>
             </div>
         </div>
     )
