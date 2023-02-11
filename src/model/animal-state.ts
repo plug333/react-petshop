63bbffd77a1f9e774aa876dc111d80d57f74a5b1
plug333 <davidemele03@gmail.com>
2023-02-11 15:38:07 +0100

    created animal-state for all state

---
+++ src/model/animal-state.ts
@@ -0,0 +1,18 @@
+import { IAnimal } from "./animal";
+
+export type TAnimalState = {
+    error: boolean;
+    loading: boolean;
+    animal: IAnimal | null;
+}
+
+export type TAnimalsState = {
+    loading: boolean;
+    error: boolean;
+    animals: IAnimal[] | null;
+}
+
+export type TAnimalDeleteState = {
+    deleting: boolean;
+    error: boolean;
+}
